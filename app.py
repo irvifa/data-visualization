d89881fb946e0329b588d759da63bc1f6d638c7d
Irvi Aini <irvi.fa@gmail.com>
2022-02-17 19:34:52 +0100

    Use pre-computed data.

--- app.py
+++ app.py
@@ -11,34 +11,25 @@ from data_visualization_utility import *
 
 app = Dash(__name__)
 
-diseases_data_prediction_input_file_path = "data/diseases.csv"
-diseases_data_prediction = get_diseases_data_prediction(
-    diseases_data_prediction_input_file_path
-)
-
-merged_disease_summary = get_merged_summary_table(diseases_data_prediction)
-merged_disease_summary = merged_disease_summary[merged_disease_summary["year"] <= 2018]
-
-
+merged_disease_summary = pd.read_csv("data/diseases-incidences/diseases_incidence_summary.csv")
 filter_disease = list(merged_disease_summary["year"].unique())
 
 app = Dash(__name__)
 app.layout = html.Div(
     children=[
         html.Div(dcc.Dropdown(filter_disease, id="filter-disease")),
-        html.Div(id="pandas-output-container-2"),
+        html.Div(id="diseases-incidence-per-year"),
     ]
 )
 
 
 @app.callback(
-    Output("pandas-output-container-2", "children"), Input("filter-disease", "value")
+    Output("diseases-incidence-per-year", "children"), Input("filter-disease", "value")
 )
 def update_output(value):
-    merged_disease_summary_on_2016 = merged_disease_summary_for_year(
-        merged_disease_summary, value
-    )
-    sm = merged_disease_summary_on_2016.groupby(["borough"], as_index=False).sum()
+    if value is None:
+        value = '2016'
+    sm = pd.read_csv("data/diseases-incidences/diseases_incidence_summary_{}.csv".format(value))
     fig = px.bar(
         sm,
         x="borough",
@@ -49,18 +40,6 @@ def update_output(value):
         ),
     )
     return dcc.Graph(id="example-graph", figure=fig)
-
-app.layout = html.Div(
-    children=[
-        html.H1(children="Data Visualization"),
-        html.Div(
-            children="""
-        Dash: A web application framework for your data.
-    """
-        ),
-        dcc.Graph(id="example-graph", figure=fig),
-    ]
-)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
